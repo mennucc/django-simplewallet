4094c0a9e39032a993c032906983b421a65f1eb6
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-11-09 18:47:19 +0100

    check `operate` in Wallet methods

--- src/django_pursed/wallet/models.py
+++ src/django_pursed/wallet/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.core.exceptions import PermissionDenied
 from .errors import InsufficientBalance
 
 
@@ -39,6 +40,9 @@ class Wallet(models.Model):
 
         Returns the transaction.
         """
+        if not self.user.has_perm('wallet.operate'):
+            raise PermissionDenied('User %r has no `operate` permission for wallet %r' %(self.user, self))
+
         T = self.transaction_set.create(
             value=value,
             running_balance=self.current_balance + value,
@@ -64,6 +68,9 @@ class Wallet(models.Model):
 
         Returns the transaction.
         """
+        if not self.user.has_perm('wallet.operate'):
+            raise PermissionDenied('User %r has no `operate` permission for wallet %r' %(self.user, self))
+
         if value > self.current_balance:
             raise InsufficientBalance('This wallet has insufficient balance.')
 
@@ -83,6 +90,9 @@ class Wallet(models.Model):
 
         Returns the two transactions, on the first and on the second wallet.
         """
+        if not self.user.has_perm('wallet.operate'):
+            raise PermissionDenied('User %r has no `operate` permission for wallet %r' %(self.user, self))
+
         T1 = self.withdraw(value, description, wallet)
         T2 = wallet.deposit(value, description, self)
         return T1,T2
