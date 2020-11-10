9fc9342e499eb50369770c9afcf3f5726641c8f7
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-11-10 08:51:20 +0100

    add wallet admin interface

--- src/django_pursed/wallet/admin.py
+++ src/django_pursed/wallet/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# https://docs.djangoproject.com/en/3.0/ref/contrib/admin/
+
+from .models import Transaction, Wallet
+
+
+class TransactionAdmin(admin.TabularInline):
+    model = Transaction
+
+class WalletAdmin(admin.ModelAdmin):
+    model = Wallet
+    inlines = [
+        TransactionAdmin,
+    ]
+    readonly_fields = ('user',)
+
+admin.site.register(Wallet, WalletAdmin)
