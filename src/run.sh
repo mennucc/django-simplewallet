b72a50afe96eb5d273b9a96adf2dd36d2caa4c91
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2020-11-11 19:34:19 +0100

    run.sh : deposit 100 to `foobar`

--- src/run.sh
+++ src/run.sh
@@ -4,5 +4,7 @@ echo Running migrations
 python3 django_pursed/manage.py migrate
 echo Creating users
 python3 django_pursed/helper.py create_fake_users
+echo Deposit some money
+python3 django_pursed/helper.py deposit foobar 100
 echo Startin server
 python3 django_pursed/manage.py runserver 0.0.0.0:8000
