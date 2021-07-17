84e5431002acd39c2ab41ea9c9343bb51ef27cc8
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2021-07-17 13:42:44 +0200

    use BASE_DIR for sqlite database

--- src/django_pursed/django_pursed/settings.py
+++ src/django_pursed/django_pursed/settings.py
@@ -93,7 +93,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(os.path.dirname(sys.argv[0]),'var/sqlite_database'),
+            'NAME': os.path.join(BASE_DIR,'var/sqlite_database'),
         }
     }
 
