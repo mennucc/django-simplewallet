6b2392d6c6fed99834fb3025b3b70114689d2c0a
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2021-07-19 16:07:22 +0200

    run.sh : add === to prints

--- src/run.sh
+++ src/run.sh
@@ -5,14 +5,14 @@ while ! python3 django_pursed/helper.py ping ; do
     sleep 2
 done
 
-echo Running migrations
 echo ======================================================== Running tests
 ( cd django_pursed ;  ./manage.py test )
 echo ======================================================== End of tests
+echo ======== Running migrations
 python3 django_pursed/manage.py migrate
-echo Creating users
+echo ======== Creating users
 python3 django_pursed/helper.py create_fake_users
-echo Deposit some money
+echo ======== Deposit some money to user foobar
 python3 django_pursed/manage.py deposit --username=foobar --amount=100
-echo Startin server
+echo ======== Starting server
 python3 django_pursed/manage.py runserver 0.0.0.0:8000
